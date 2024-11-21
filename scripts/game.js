9e9f041c2804a1e7f1d1b55c612590238a877615
Belceb45 <rubiodiego001@gmail.com>
2024-11-21 12:58:15 -0600

    Correcion de espacios, title-neon h2

--- scripts/game.js
+++ scripts/game.js
@@ -14,7 +14,7 @@ titulo.style.marginTop = '5rem';
 const tituloh2 = document.querySelector('.title-neon h2');
 tituloh2.style.color = '#c71585';
 tituloh2.style.textShadow = '0 0 5px #c71585, 0 0 10px #c71585, 0 0 30px #c71585, 0 0 50px #c71585, 0 0 70px #c71585';
-tituloh2.style.fontSize = '3rem';
+tituloh2.style.fontSize = '5rem';
 
 const container = document.querySelector('.container');
 container.style.marginBottom = '2rem';
